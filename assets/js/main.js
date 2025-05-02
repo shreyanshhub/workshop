8743162f73057dad8169550d2e8aa71d4bdcd641
shreyanshhub <21f1003357@student.onlinedegree.iitm.ac.in>
2025-05-02 16:08:22 +0530

    Fixed everything

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Theme toggle
     const themeToggle = document.querySelector('.theme-toggle');
-    const currentTheme = localStorage.getItem('theme') || 'dark';
+    const currentTheme = localStorage.getItem('theme') || 'light';
     document.documentElement.setAttribute('data-theme', currentTheme);
 
     themeToggle.addEventListener('click', () => {
-        const newTheme = document.documentElement.getAttribute('data-theme') === 'light' ? 'dark' : 'ligh';
+        const newTheme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
         document.documentElement.setAttribute('data-theme', newTheme);
         localStorage.setItem('theme', newTheme);
     });
